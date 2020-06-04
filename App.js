9d35acff82128686682abbd4fa7dbae94e3a059e
DacHuy <nguyendachuy5326@gmail.com>
2020-06-04 16:08:21 +0700

    Add Files

---
+++ App.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+export default class App extends React.Component{
+    render(){
+      return(
+        <View>
+          <Text>Dammit</Text>
+        </View>
+      )
+    }
+}
